64f6596ccc13e3d0fdf0afa38505025724d4c256
Babur <baburnawyan@gmail.com>
2025-11-08 20:32:23 -0500

    Delete warning method related code

--- src/js/background.js
+++ src/js/background.js
@@ -4,11 +4,9 @@ It also forwards messages from popup.js to capture.js such as the webcam selecte
 and the warning method selected. Lastly, this file sends a desktop notification or blur and unblur browser tabs
 after receiving messages from capture.js to warn the user about their bad posture.
 */
-let captureIsReady, captureTabId, currentSelectedWebcam, currentProcessingSpeed, currentWarningMethod, currentActivity, 
-    firstWebcamNotSentToCapture, firstProcessingSpeedNotSentToCapture, firstWarningMethodNotSentToCapture, 
-    firstActivityNotSentToCapture, isBlurred;
+let captureIsReady, captureTabId, currentSelectedWebcam, currentProcessingSpeed, currentActivity, 
+    firstWebcamNotSentToCapture, firstProcessingSpeedNotSentToCapture, firstActivityNotSentToCapture;
 const readyTabs = new Set();
-
 
 function initializeGlobalVariables() {
     const variablesToInit = {
@@ -16,13 +14,10 @@ function initializeGlobalVariables() {
         captureTabId: null,
         currentSelectedWebcam: undefined,
         currentProcessingSpeed: undefined,
-        currentWarningMethod: undefined,
         currentActivity: undefined,
         firstWebcamNotSentToCapture: undefined,
         firstProcessingSpeedNotSentToCapture: undefined,
-        firstWarningMethodNotSentToSandbox: undefined,
-        firstActivityNotSentToCapture: undefined,
-        isBlurred: false
+        firstActivityNotSentToCapture: undefined
     };
 
     chrome.storage.local.get(Object.keys(variablesToInit), (result) => {
@@ -30,7 +25,6 @@ function initializeGlobalVariables() {
         captureTabId = result.hasOwnProperty('captureTabId') ? result.captureTabId : variablesToInit.captureTabId;
         currentSelectedWebcam = result.hasOwnProperty('currentSelectedWebcam') ? result.currentSelectedWebcam : variablesToInit.currentSelectedWebcam;
         currentProcessingSpeed = result.hasOwnProperty('currentProcessingSpeed') ? result.currentProcessingSpeed : variablesToInit.currentProcessingSpeed;
-        currentWarningMethod = result.hasOwnProperty('currentWarningMethod') ? result.currentWarningMethod : variablesToInit.currentWarningMethod;
         currentActivity = result.hasOwnProperty('currentActivity') ? result.currentActivity : variablesToInit.currentActivity;
         if (result.hasOwnProperty('firstWebcamNotSentToCapture')) {
             firstWebcamNotSentToCapture = result.firstWebcamNotSentToCapture;
@@ -42,17 +36,11 @@ function initializeGlobalVariables() {
         } else {
             firstProcessingSpeedNotSentToCapture = variablesToInit.firstProcessingSpeedNotSentToCapture;
         }
-        if (result.hasOwnProperty('firstWarningMethodNotSentToCapture')) {
-            firstWarningMethodNotSentToCapture = result.firstWarningMethodNotSentToCapture;
-        } else {
-            firstWarningMethodNotSentToCapture = variablesToInit.firstWarningMethodNotSentToCapture;
-        }
         if (result.hasOwnProperty('firstActivityNotSentToCapture')) {
             firstActivityNotSentToCapture = result.firstActivityNotSentToCapture;
         } else {
             firstActivityNotSentToCapture = variablesToInit.firstActivityNotSentToCapture;
         }
-        isBlurred = result.hasOwnProperty('isBlurred') ? result.isBlurred : variablesToInit.isBlurred;
 
         // Special handling for captureTabId
         if (captureTabId !== null) {
@@ -82,35 +70,20 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
         }
     } else if (message.type === 'sendNotification') {
         sendWarningNotification();
-    } else if (message.type === "blurScreen") {
-        setBlurState(true);
-    } else if (message.type === 'unblurScreen') {
-        setBlurState(false);
     } else if (message.type === 'processingSpeed' && message.processingSpeed !== currentProcessingSpeed) {
         sendSpeedInfoToCapture(message.processingSpeed);
-    } else if (message.type === 'warningMethod' && message.warningMethod !== currentWarningMethod) {
-        sendWarningInfoToCapture(message.warningMethod);
     } else if (message.type === 'activity' && message.activity !== currentActivity) {
         sendActivityInfoToCapture(message.activity);
     } else if (message.type === 'captureIsReady') {
         setCaptureToReady();
     } else if (message.type === 'captureIsReadyToClose') {
-        if (isBlurred) {
-            setBlurState(false);
-        }
         closeCaptureTab();
-    } else if (message.action === 'contentScriptReady') {
-        // readyTabs.add(sender.tab.id);
-        if (isBlurred) {
-            applyBlurToTab(sender.tab.id, true)
-        }
     } else if (message.type === 'webglContextLost') {
         sendErrorOccuredNotification();
     } else if (message.type === 'webglContextRestored') {
         sendErrorResolvedNotification();
     }
 });
-
 
 function sendWebcamInfoToCapture(webcamId) {
     currentSelectedWebcam = webcamId;
@@ -123,7 +96,6 @@ function sendWebcamInfoToCapture(webcamId) {
     }
 }
 
-
 function sendSpeedInfoToCapture(processingSpeed) {
     currentProcessingSpeed = processingSpeed;
     chrome.storage.local.set({ currentProcessingSpeed: currentProcessingSpeed });
@@ -134,19 +106,6 @@ function sendSpeedInfoToCapture(processingSpeed) {
         chrome.storage.local.set({ firstProcessingSpeedNotSentToCapture: firstProcessingSpeedNotSentToCapture });
     }
 }
-
-
-function sendWarningInfoToCapture(warningMethod) {
-    currentWarningMethod = warningMethod;
-    chrome.storage.local.set({ currentWarningMethod: currentWarningMethod });
-    if (captureIsReady) {
-        chrome.runtime.sendMessage({ type: 'warningMethod', warningMethod: currentWarningMethod });
-    } else {
-        firstWarningMethodNotSentToCapture = true;
-        chrome.storage.local.set({ firstWarningMethodNotSentToCapture: firstWarningMethodNotSentToCapture });
-    }
-}
-
 
 function sendActivityInfoToCapture(activity) {
     currentActivity = activity;
@@ -159,7 +118,6 @@ function sendActivityInfoToCapture(activity) {
     }
 }
 
-
 function setCaptureToReady() {
     captureIsReady = true;
     chrome.storage.local.set({ captureIsReady: captureIsReady });
@@ -169,14 +127,10 @@ function setCaptureToReady() {
     if (firstProcessingSpeedNotSentToCapture === true) {
         chrome.runtime.sendMessage({ type: 'processingSpeed', processingSpeed: currentProcessingSpeed });
     }
-    if (firstWarningMethodNotSentToCapture === true) {
-        chrome.runtime.sendMessage({ type: 'warningMethod', warningMethod: currentWarningMethod });
-    }
     if (firstActivityNotSentToCapture === true) {
         chrome.runtime.sendMessage({ type: 'activity', activity: currentActivity });
     }
 }
-
 
 chrome.tabs.onRemoved.addListener((tabId, removeInfo) => {
     if (tabId === captureTabId) {
@@ -187,7 +141,6 @@ chrome.tabs.onRemoved.addListener((tabId, removeInfo) => {
         chrome.storage.local.set({ extensionIsOn: false });
     }
 });
-
 
 function openCaptureTab() {
     // Use chrome.runtime.getURL to get the full extension URL from the root
@@ -204,7 +157,6 @@ function openCaptureTab() {
     });
 }
 
-
 function closeCaptureTab() {
     if (captureTabId) {
         chrome.tabs.remove(captureTabId);
@@ -212,20 +164,16 @@ function closeCaptureTab() {
     }
 }
 
-
 function resetVariables() {
     const keysToRemove = [
         'captureIsReady',
         'captureTabId',
         'currentSelectedWebcam',
         'currentProcessingSpeed',
-        'currentWarningMethod',
         'currentActivity',
         'firstWebcamNotSentToCapture',
         'firstProcessingSpeedNotSentToCapture',
-        'firstWarningMethodNotSentToSandbox',
-        'firstActivityNotSentToCapture',
-        'isBlurred'
+        'firstActivityNotSentToCapture'
     ];
 
     chrome.storage.local.remove(keysToRemove, () => {
@@ -237,17 +185,13 @@ function resetVariables() {
             captureTabId = null;
             currentSelectedWebcam = null;
             currentProcessingSpeed = null;
-            currentWarningMethod = null;
             currentActivity = null;
             firstWebcamNotSentToCapture = null;
             firstProcessingSpeedNotSentToCapture = null;
-            firstWarningMethodNotSentToCapture = null;
             firstActivityNotSentToCapture = null;
-            isBlurred = false;
         }
     });
 }
-
 
 function sendWarningNotification() {
     chrome.notifications.create('warningNotification', {
@@ -258,7 +202,6 @@ function sendWarningNotification() {
         iconUrl: '../../icons/bird128.png'
     });
 }
-
 
 function sendErrorOccuredNotification() {
     chrome.notifications.create('warningNotification', {
@@ -271,7 +214,6 @@ function sendErrorOccuredNotification() {
     });
 }
 
-
 function sendErrorResolvedNotification() {
     chrome.notifications.create('warningNotification', {
         type: 'basic',
@@ -281,47 +223,3 @@ function sendErrorResolvedNotification() {
         iconUrl: '../../icons/bird128.png'
     });
 }
-
-
-function setBlurState(blur) {
-    isBlurred = blur;
-    chrome.storage.local.set({ isBlurred: isBlurred });
-    chrome.tabs.query({}, (tabs) => {
-        tabs.forEach(tab => {
-            applyBlurToTab(tab.id, tab.title, blur);
-            // ensureContentScriptAndApplyBlur(tab.id, tab.title, blur);
-        });
-    });
-}
-
-
-function applyBlurToTab(tabId, tabTitle, blur) {
-    chrome.tabs.sendMessage(tabId, { action: blur ? 'blur' : 'unblur' })
-        .catch(() => {
-            // If there's an error, inject the content script and try again
-            chrome.scripting.executeScript({
-                target: { tabId: tabId },
-                files: ['content.js']
-            }).then(() => {
-                // After injection, wait a bit and try to send the message again
-                setTimeout(() => {
-                    chrome.tabs.sendMessage(tabId, { action: blur ? 'blur' : 'unblur' })
-                        .catch(error => console.log(`Error sending message to tab ${tabTitle} after injection:`, error));
-                }, 100);
-            }).catch(error => console.log(`Error injecting script into tab ${tabTitle}:`, error));
-        });
-}
-
-
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status === 'complete' && isBlurred) {
-        applyBlurToTab(tabId, true);
-    }
-});
-
-
-chrome.runtime.onStartup.addListener(() => {
-    if (isBlurred) {
-        setBlurState(true);
-    }
-});
